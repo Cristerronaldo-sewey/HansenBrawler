ee6c71ca3fa58bcb6e7da7616dc1811791cb7120
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2023-09-21 12:52:32 +0200

    Kong Elvis #4 Headbutt yes

--- Assets/Elvis script.cs
+++ Assets/Elvis script.cs
@@ -9,6 +9,40 @@ public class KongElvis : Player
     public Foot LeftFoot;
     public Foot RightFoot;
     public Body MainBody;
-   
-   public Head BodyHead;
+
+    public Head BodyHead;
+
+    //Move forward method 
+    public void MoveForward()
+    {
+        //Move forward
+        transform.Translate(Vector3.forward  * Time.deltaTime);
+    }
+    //Move backward method 
+    public void MoveBackward()
+    {
+        //Move Backward
+        transform.Translate(Vector3.back  * Time.deltaTime);
+
+    }
+
+    public void Update()
+    {
+        // Detect when you want to trigger the punch animation, for example, by pressing a button.
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            // Set the "Punch" trigger to transition to the punch animation state.
+            Punch();
+        }
+    }
+
+    public void Punch()
+    {
+        //punch
+        Animator animator = GetComponent<Animator>();
+
+        animator.SetTrigger("Punch");
+
+    }
+
 }
