9c20bc143694294095a6524d536be739ce32b5c2
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2023-09-21 11:19:30 +0200

    Kong elvis #3

---
+++ Assets/Elvis script.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KongElvis : Player
+{
+    public Hand LeftHand;
+    public Hand RightHand;
+    public Foot LeftFoot;
+    public Foot RightFoot;
+    public Body MainBody;
+   
+   public Head BodyHead;
+}
